bddaab7f8d83ec7c871b717a1fd83c6dd29baadb
s_Lee <49893333+smlee5270@users.noreply.github.com>
2022-01-09 15:10:07 +0900

    Update stack.c

--- stack.c
+++ stack.c
@@ -1,59 +1,67 @@
-#include <stdio.h>
-#define STACK_SIZE 10  
- 
-int stack[STACK_SIZE];    
-int top = -1;   
- 
-int isFull(){
-    if(top >= STACK_SIZE -1){    
-        printf("Error : Stack is Full!\n");
-        return 1;
-    }
-    return 0;
-}
-int isEmpty(){
-    if(top == -1){
-        printf("Error : Stack is empty!\n");
-        return 1;
-    }
-    return 0;
-}
- 
-void push(int data){
-    if(!isFull()){
-        top++;
-        stack[top] = data;
-    }
-} 
- 
-int pop(){
-    if(!isEmpty()){
-        return stack[top--];
-    }
-}
- 
- 
-void printStack(){
-    int i;
-    for(i=0; i<=top; i++){
-        printf("%d ", stack[i]);
-    }
-    printf("\n");
-}
- 
-int main(){
-    
-    push(5);
-    push(4);
-    push(3);
-    push(2);
-    push(1);
-    printStack();
-    pop();
-    pop();
-	pop();
-    printStack();
-  
-    return 0;
+#include<stdio.h>
+#define MAX 5
+
+int stack[MAX];
+int top = -1;
+
+
+int Empty(){
+	
+	if(top == -1){
+		printf("비었음\n");
+		return 1; 
+		
+	} else {
+		return 0;
+	} 
 }
 
+
+int Full(){
+	
+	if(top >= MAX-1){
+		printf("꽉참\n");
+		return 1;
+		
+	} else {
+		return 0;
+	}
+}
+
+
+int Push(int data){
+	
+	if(Full()) {
+		return 0;
+	} else {
+		stack[++top] = data;
+	}
+	
+}
+
+
+int Pop(){
+	
+	if(Empty()){
+		return 0;
+	} else {
+		printf("%d\n", stack[top--]);
+	}
+	
+}
+
+
+int main(void){
+	
+	Push(1);
+	Push(2);
+	Push(3);
+	Push(4);
+	Push(5);
+	Push(6);
+	Pop();
+	Pop();
+	Pop();
+	Pop();
+	Pop();
+}
